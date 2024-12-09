e77591ee7c7c68b58af5165cf35c687c16ddd84a
Bishozit <bishozitdas191@gmail.com>
2024-12-09 18:57:54 +0600

    Implement the logout dialog

--- lib/screens/user.dart
+++ lib/screens/user.dart
@@ -128,7 +128,9 @@ class _UserScreenState extends State<UserScreen> {
               _listTiles(
                 title: 'Logout',
                 icon: IconlyLight.logout,
-                onPressed: () {},
+                onPressed: () {
+                  _showLogoutDialog();
+                },
                 color: color,
               ),
             ],
@@ -136,6 +138,50 @@ class _UserScreenState extends State<UserScreen> {
         )),
       ),
     );
+  }
+
+  Future<void> _showLogoutDialog() async {
+    await showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Row(children: [
+              Image.asset(
+                'assets/images/warning_sign.png',
+                height: 20,
+                width: 20,
+                fit: BoxFit.fill,
+              ),
+              const SizedBox(
+                width: 8,
+              ),
+              const Text('Sign Out')
+            ]),
+            content: const Text('Do you wanna sign out?'),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  if (Navigator.canPop(context)) {
+                    Navigator.pop(context);
+                  }
+                },
+                child: TextWidget(
+                  text: 'Cancle',
+                  color: Colors.cyan,
+                  textSize: 18,
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: TextWidget(
+                  text: 'Ok',
+                  color: Colors.red,
+                  textSize: 18,
+                ),
+              )
+            ],
+          );
+        });
   }
 
   Future<void> _showAddressDialog() async {
